3025323c6320e89f7978a005e59b1d03d3d6bd6d
Parvendra <parvendra.kumar@gmail.com>
2023-08-25 22:32:03 +0530

    Initial commit

---
+++ CombineWithSwiftUI/CombineWithSwiftUI/CombineWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineWithSwiftUIApp.swift
+//  CombineWithSwiftUI
+//
+//  Created by Parvendra Kumar on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
